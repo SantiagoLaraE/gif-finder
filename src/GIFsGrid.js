acee64a0166c5bcc26e63d73f5267cb43fcd5eb5
SantiagoLaraE <oscarescobar773@gmail.com>
2022-12-13 11:47:38 -0500

    Ajuste Lógica de funcionamiento de grid

---
+++ src/GIFsGrid.js
@@ -0,0 +1,46 @@
+let columns;
+const gap = 8;
+let columnHeights = {};
+
+function setColumns(wrapperWidth) {
+  if (wrapperWidth > 1080) {
+    columns = 4;
+  } else if (wrapperWidth > 768) {
+    columns = 3;
+  } else {
+    columns = 2;
+  }
+}
+
+export function setWrapperHeight(wrapper) {
+  const itemsHeights = Object.values(columnHeights);
+  const highest =
+    itemsHeights.reduce((acc, cur) => (acc < cur ? cur : acc), 0) + gap;
+  wrapper.style.height = `${highest}px`;
+}
+
+export function setGIFWidth(wrapperWidth) {
+  setColumns(wrapperWidth);
+  return (wrapperWidth - gap * (columns - 1)) / columns;
+}
+
+export function setGIFTranslate(index, width, height) {
+  const translateX = (index % columns) * width + (index % columns) * gap;
+  const translateY = columnHeights[`column-${index % columns}`] + gap || 0;
+
+  setColumnsHeights(index, height);
+
+  return `translateX(${translateX}px) translateY(${translateY}px)`;
+}
+
+export function resetColumnHeights() {
+  columnHeights = {};
+}
+
+function setColumnsHeights(index, height) {
+  if (columnHeights[`column-${index % columns}`]) {
+    columnHeights[`column-${index % columns}`] += height + gap;
+  } else {
+    columnHeights[`column-${index % columns}`] = height;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
+import { setWrapperHeight, setGIFWidth, setGIFTranslate, resetColumnHeights } from "./GIFsGrid.js";
 
 const wrapper = document.querySelector(".gifs__wrapper");
 let wrapperWidth = wrapper.clientWidth;
-const gap = 8;
-let columns;
-const columnsHeights = {};
 const API_KEY = "2STJgtp7HDg3PAulHpsetnjTzGxkVzUk";
-
-
 
 async function getTrendingGIFs() {
   const response = await fetch(
@@ -17,84 +13,49 @@ async function getTrendingGIFs() {
 }
 
 function createGIFs(GIFs) {
-  setColumns();
   const fragment = new DocumentFragment();
-  const newWidth = (wrapperWidth - gap * (columns - 1)) / columns;
-  GIFs.map((item, index) => {
-    const imgWidth = item.images.fixed_width.width;
-    const imgHeight = item.images.fixed_width.height;
-    const newHeight = (newWidth * imgHeight) / imgWidth;
-    const translateX = (index % columns) * newWidth + (index % columns) * gap;
-    const translateY = Math.ceil(columnsHeights[`column-${index % columns}`] + gap) || 0;
+  const GIFWidth = setGIFWidth(wrapperWidth);
+  GIFs.map((GIF, index) => {
+    const imgWidth = GIF.images.fixed_width.width;
+    const imgHeight = GIF.images.fixed_width.height;
+    const GIFHeight = (GIFWidth * imgHeight) / imgWidth;
 
     const article = document.createElement("article");
     article.classList.add("gif");
-    article.style.width = `${newWidth}px`;
-    article.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`;
+    article.style.width = `${GIFWidth}px`;
+    article.style.transform = setGIFTranslate(index, GIFWidth, GIFHeight);
 
     const picture = document.createElement("picture");
 
     const img = document.createElement("img");
     img.classList.add("gif-img");
-    img.src = item.images.fixed_width.url;
+    img.src = GIF.images.fixed_width.url;
 
     picture.appendChild(img);
     article.appendChild(picture);
     fragment.appendChild(article);
 
-    if (columnsHeights[`column-${index % columns}`]) {
-      columnsHeights[`column-${index % columns}`] += newHeight + gap;
-    } else {
-      columnsHeights[`column-${index % columns}`] = newHeight;
-    }
   });
   wrapper.appendChild(fragment);
-  const itemsHeights = Object.values(columnsHeights);
-  const height =
-    itemsHeights.reduce((acc, cur) => (acc < cur ? cur : acc), 0) + gap;
-  wrapper.style.height = `${height}px`;
+  setWrapperHeight(wrapper);
 }
 
 getTrendingGIFs();
+
+
+function setGrid() {
+  const GIFs = document.querySelectorAll(".gif");
+  const GIFWidth = setGIFWidth(wrapperWidth);
+  resetColumnHeights();
+  GIFs.forEach((GIF, index) => {
+    GIF.style.width = `${GIFWidth}px`;
+    GIF.style.transform = setGIFTranslate(index, GIFWidth, GIF.clientHeight);
+  });
+
+  setWrapperHeight(wrapper);
+}
 
 window.addEventListener("resize", () => {
   wrapperWidth = wrapper.clientWidth;
   setGrid();
 });
-
-function setGrid() {
-  setColumns();
-
-  const gifs = document.querySelectorAll(".gif .gif-img");
-  const width = (wrapperWidth - gap * (columns - 1)) / columns;
-  const data = {};
-  gifs.forEach((item, index) => {
-    const translateX = (index % columns) * width + (index % columns) * gap;
-    const translateY = data[`columna-${index % columns}`] + gap || 0;
-
-    item.offsetParent.style.width = `${width}px`;
-    item.offsetParent.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`;
-
-    if (data[`columna-${index % columns}`]) {
-      data[`columna-${index % columns}`] += item.clientHeight + gap;
-    } else {
-      data[`columna-${index % columns}`] = item.clientHeight;
-    }
-  });
-
-  const itemsHeights = Object.values(data);
-  const height =
-    itemsHeights.reduce((acc, cur) => (acc < cur ? cur : acc), 0) + gap;
-  wrapper.style.height = `${height}px`;
-}
-
-
-function setColumns() {
-  if (wrapperWidth > 1080) {
-    columns = 4;
-  } else if (wrapperWidth > 768) {
-    columns = 3;
-  } else {
-    columns = 2;
-  }
-}
